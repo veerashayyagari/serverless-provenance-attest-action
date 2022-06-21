582f8e4d7c42b4de3739ec6ebeb86a37fc39b2e3
Veerash Ayyagari <veeray@microsoft.com>
2022-06-20 17:35:36 -0700

    implement original action for attestation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');    
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const outputPath: string = core.getInput('publish-output-path');    
+    core.debug(`Received output path ${outputPath} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const repo: string = process.env.GITHUB_REPOSITORY ?? 'Unknown';
+    const owner:string = process.env.GITHUB_REPOSITORY_OWNER ?? 'Unknown';
+    const absolutePath: string = path.resolve(outputPath, '.attest.txt');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Absolute Path : ${absolutePath}`);
+    core.debug(`Process.env : ${JSON.stringify(process.env)}`);    
 
-    core.setOutput('time', new Date().toTimeString())
+    fs.writeFile(`${absolutePath}`, `${repo}##${owner}`, { flag: 'a+' }, err => { if(err) { core.setFailed(`${err?.message}, creating file`); } });
+    core.debug(new Date().toTimeString());
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
